c26d743226409a774b8cd2cb4550ca20487a59f9
amoresca <casee.amores@gmail.com>
2021-01-16 09:58:45 -0500

    adds route using segment parameters

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
-
   namespace :api do
     get "/shout_it" => "practice_examples#shout"
+    get "/shout_it/:phrase" => "practice_examples#shout"
   end
 end
